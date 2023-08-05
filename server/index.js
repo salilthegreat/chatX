671ad9f190f692683caeb3c09c64815b417a1eb9
salilthegreat <salilkumardas22@gmail.com>
2023-08-04 22:18:44 -0700

    Basic server setup done

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const app = express()
+const cors = require("cors")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+dotenv.config();
+
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.get("/",(req,res)=>{
+    console.log("ram")
+})
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("MongoDB connected successfully")
+}).catch((error)=>{
+    console.log(error)
+})
+
+app.listen(5000,()=>{
+    console.log("App is listening")
+})
